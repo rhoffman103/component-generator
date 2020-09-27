92cdf21e72ca1c4e271f65a66aa0951b6f1ecfbd
Bobby H <rhoffman103@gmail.com>
2020-09-27 18:19:26 -0400

    generate view or component

---
+++ src/config/handlebars.ts
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+import handlebars from "handlebars";
+
+handlebars.registerHelper('ifIn', function(this: any, elem, list, options) {
+  if(list.indexOf(elem) > -1) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+const partialNames = fs
+  .readdirSync(path.join(__dirname, '..', 'templates', 'partials'))
+  .filter((file) => file.includes('.handlebars'));
+
+for (const file of partialNames) {
+  const template = fs.readFileSync(path.join(__dirname, '..', 'templates', 'partials', file), "utf-8");
+  handlebars.registerPartial(file.replace(".handlebars", ""), template);
+}
+
+export const outputPath = path.join(__dirname, "..", "..", "client");
+
+export default handlebars;
